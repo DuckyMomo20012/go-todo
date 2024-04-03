d51cd7c63e4473d7ef19e0e5a568834a27848372
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-03 23:00:28 +0700

    feat: setup task adapters

---
+++ internal/tasks/adapters/task_pg_repositoy.go
@@ -0,0 +1,49 @@
+package adapters
+
+import (
+	"context"
+
+	"github.com/DuckyMomo20012/go-todo/internal/tasks/app"
+	"xorm.io/xorm"
+)
+
+type PgTaskRepository struct {
+	engine *xorm.Engine
+}
+
+func NewPgTaskRepository(engine *xorm.Engine) *PgTaskRepository {
+	return &PgTaskRepository{engine: engine}
+}
+
+func (p PgTaskRepository) GetAll(ctx context.Context) ([]app.Task, error) {
+	var tasks []app.Task
+	err := p.engine.Find(&tasks)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (p PgTaskRepository) Create(ctx context.Context, task *app.Task) error {
+	_, err := p.engine.Insert(task)
+	return err
+}
+
+func (p PgTaskRepository) Delete(ctx context.Context, id string) error {
+	_, err := p.engine.ID(id).Delete(&app.Task{})
+	return err
+}
+
+func (p PgTaskRepository) GetById(ctx context.Context, id string) (*app.Task, error) {
+	var task app.Task
+	_, err := p.engine.ID(id).Get(&task)
+	if err != nil {
+		return nil, err
+	}
+	return &task, nil
+}
+
+func (p PgTaskRepository) Update(ctx context.Context, id string, task *app.Task) error {
+	_, err := p.engine.ID(id).Update(task)
+	return err
+}
