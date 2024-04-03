8a1675836214ac64c70698ae2d1adff18a0e0cbd
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-03 23:01:28 +0700

    feat: setup config files

---
+++ internal/tasks/configs/cfg.go
@@ -0,0 +1,11 @@
+package configs
+
+type ServerConfig struct {
+	Host   string `mapstructure:"HOST"`
+	Port   string `mapstructure:"PORT"`
+	DbHost string `mapstructure:"DB_HOST"`
+	DbPort string `mapstructure:"DB_PORT"`
+	DbUser string `mapstructure:"DB_USER"`
+	DbPass string `mapstructure:"DB_PASSWORD"`
+	DbName string `mapstructure:"DB_NAME"`
+}
