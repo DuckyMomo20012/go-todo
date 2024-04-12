18d10c6e8df3a111b8cb56711294bcfa2d8115f1
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-12 23:36:33 +0700

    feat: setup grpc server

---
+++ internal/common/server/grpc.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+func RunGRPCServer(registerServer func(server *grpc.Server)) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	grpcEndpoint := fmt.Sprintf(":%s", port)
+
+	grpcServer := grpc.NewServer()
+	registerServer(grpcServer)
+
+	listen, err := net.Listen("tcp", grpcEndpoint)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.WithField("grpcEndpoint", grpcEndpoint).Info("Starting: gRPC Listener")
+	logrus.Fatal(grpcServer.Serve(listen))
+}
