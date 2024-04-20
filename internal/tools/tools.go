00ace10aa911c0c96cf88da821a184acc19491e4
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-20 20:25:20 +0700

    chore(build): remove oapi-codegen dep

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -4,7 +4,6 @@
 package tools
 
 import (
-	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"               // OpenAPI 3.0 code generator
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"               // Linter
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway" // gRPC Gateway
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"    // gRPC Gateway
