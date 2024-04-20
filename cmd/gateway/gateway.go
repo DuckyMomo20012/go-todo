4927fd0988f849b26d76045f9c534e7e6bb59743
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-20 22:09:49 +0700

    feat(cmd): add cobra cli

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -1,4 +1,4 @@
-package main
+package gateway
 
 import (
 	"context"
@@ -11,12 +11,34 @@ import (
 	"github.com/DuckyMomo20012/go-todo/internal/gateway/configs"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.uber.org/multierr"
 	"google.golang.org/grpc"
 )
 
-func main() {
+func NewGatewayCmd() *cobra.Command {
+	gatewayCmd := &cobra.Command{
+		Use:   "gateway",
+		Short: "Gateway server commands.",
+		Long:  "Gateway server commands.",
+	}
+
+	startCmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start the gateway server.",
+		Long:  "Start the gateway server. This will start the HTTP gateway proxy server.",
+		Run: func(cmd *cobra.Command, args []string) {
+			startGatewayServer()
+		},
+	}
+
+	gatewayCmd.AddCommand(startCmd)
+
+	return gatewayCmd
+}
+
+func startGatewayServer() {
 	viper.SetDefault("HOST", "0.0.0.0")
 	viper.SetDefault("PORT", "8081")
 
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/DuckyMomo20012/go-todo/cmd"
+
+func main() {
+	cmd.Execute()
+}
