414769aa0306d8427018da47f0cada3bade51b69
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-04-20 15:42:45 +0700

    feat(grpc): enable reflection service

--- internal/common/server/grpc.go
+++ internal/common/server/grpc.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func RunGRPCServer(registerServer func(server *grpc.Server)) {
@@ -16,6 +17,13 @@ func RunGRPCServer(registerServer func(server *grpc.Server)) {
 
 	grpcServer := grpc.NewServer()
 	registerServer(grpcServer)
+
+	// NOTE: This may be a security risk, as it allows anyone to see the
+	// services available on the server. It is recommended to remove this in a
+	// production environment.
+	// Currently, it is enabled for Postman testing purposes.
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
 
 	listen, err := net.Listen("tcp", grpcEndpoint)
 	if err != nil {
